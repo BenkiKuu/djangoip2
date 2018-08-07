93b8afb0a4dea0959f0233baf97b61a896a81ec8
BenkiKuu <iganeleo@gmail.com>
2018-08-07 21:11:35 +0300

    registers the admin page

---
+++ instagram/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Article,Profile
+
+# admin.site.register(Editor)
+admin.site.register(Article)
+# admin.site.register(tags)
+admin.site.register(Profile)
