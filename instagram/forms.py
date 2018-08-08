269a6ebbf68264da65dc627d163335f81fc59357
BenkiKuu <iganeleo@gmail.com>
2018-08-08 11:58:43 +0300

    Creates a news letter form

---
+++ instagram/forms.py
@@ -0,0 +1,7 @@
+from .models import Article, Profile
+from django import forms
+
+
+class NewsLetterForm(forms.Form):
+    your_name=forms.CharField(label='First Name',max_length=30)
+    email=forms.EmailField(label='Email')
