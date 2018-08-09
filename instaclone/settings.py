db2e9c7292b1619a352568115ea8c577215655fd
BenkiKuu <iganeleo@gmail.com>
2018-08-09 12:46:35 +0300

    installs whitenoise

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -16,11 +16,8 @@ from whitenoise.django import DjangoWhiteNoise
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -47,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,7 +127,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instaclone.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
 
 #Email configurations remember to install python-decouple
 # EMAIL_USE_TLS = config('EMAIL_USE_TLS')
